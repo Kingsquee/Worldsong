ec454732c5203e1fe6b150c81eacfe9144b86f50
Kingsley Tolmie <kingsley.tolmie@gmail.com>
2014-12-27 19:34:02 -0800

    Removed clean.sh

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "";
-echo "Removing target directories";
-find . -maxdepth 3 -name target -type d | xargs rm -rf
-echo "Done";
-echo "";
-echo "Removing Cargo locks";
-rm -f ./common/Cargo.lock;
-echo "Done";
